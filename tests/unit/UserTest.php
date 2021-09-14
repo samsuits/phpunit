b5cec1a90d91142baebca2f49cdcf0149b533e89
Sftp User <sftp_user@ip-172-31-30-140.us-west-2.compute.internal>
2021-09-14 20:36:27 +0000

    more user tests

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -2,21 +2,69 @@
 
 class UserTest extends \PHPUnit\Framework\TestCase
 {
-    public function testThatWeCnGetTheFirstName()
+    protected $user;
+
+    protected function setUp(): void
     {
-        $user = new \App\Models\User;
+        $this->user = new \App\Models\User;
 
-        $user->setFirstName("Billy");
+    }
+    public function testThatWeCnGetTheFirstName(): void
+    {
+        $this->user->setFirstName("Sam");
 
-        $this->assertEquals($user->getFirstName(),'Billy');
+        $this->assertEquals($this->user->getFirstName(),'Sam');
     }
 
-    public function testThatWeCnGetTheLastName()
+    public function testThatWeCnGetTheLastName(): void
     {
-        $user = new \App\Models\User;
+        $this->user->setLastName("Work");
 
-        $user->setLastName("Garrett");
+        $this->assertEquals($this->user->getLastName(),'Work');
+    }
 
-        $this->assertEquals($user->getLastName(),'Garrett');
+    public function testFullNameIsReturned(): void
+    {
+
+        $this->user->setFirstName('Sam');
+        $this->user->setLastName('Work');
+
+        $this->assertEquals($this->user->getFullName(),'Sam Work');
+
+    }
+
+    public function testFirstAndLastNameAreTrimmed(): void
+    {
+        $this->user->setFirstName('  Sam  ');
+        $this->user->setLastName('      Work ');
+
+        $this->assertEquals($this->user->getFirstName(),'Sam');
+        $this->assertEquals($this->user->getLastName(),'Work');
+
+    }
+
+    public function testEmailAddressCanBeSet(): void
+    {
+        $this->user->setEmail('sam@samwork.co.nz');
+
+        $this->assertEquals($this->user->getEmail(),'sam@samwork.co.nz');
+
+    }
+
+    public function testEmailVariablesContainCorrectValues(): void
+    {
+        $this->user->setFirstName('Sam');
+        $this->user->setLastName('Work');
+        $this->user->setEmail('sam@samwork.co.nz');
+
+        $emailVariables = $this->user->getEmailVariables();
+
+        $this->assertArrayHasKey('full_name',$emailVariables);
+        $this->assertArrayHasKey('email',$emailVariables);
+
+        $this->assertEquals($emailVariables['full_name'],'Sam Work');
+        $this->assertEquals($emailVariables['email'],'sam@samwork.co.nz');
+
+
     }
 }
